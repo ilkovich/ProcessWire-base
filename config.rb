ae95468d7e6de8e2327eb658510ba07b9e6d9f34
Daniel Ilkovich <dilkovich@thoughtvineinc.com>
2013-04-30 18:12:15 -0400

    relative assets

--- config.rb
+++ config.rb
@@ -2,6 +2,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
+http_images_path = "/img/"
 css_dir = "./webroot/css"
 sass_dir = "./styles/"
 images_dir = "./webroot/img"
@@ -12,7 +13,7 @@ fonts_dir = "./webroot/fonts"
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
